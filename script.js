71ab333defc7116f5138d830938cb58cc280dd37
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-11-12 14:42:30 -0500

    fixed script.js errors

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let saveIconBtn = $(". save-icon");
+let saveIconBtn = $(".save-icon");
 let container =$(".container");
-let time9am = $(document.createElementById('nine'))
-let time10am = $(document.createElementById('ten'))
-let time11am = $(document.createElementById('eleven'))
-let time12pm = $(document.createElementById('tweleve'))
-let time1pm = $(document.createElementById('one'))
-let time2pm = $(document.createElementById('two'))
-let time3pm = $(document.createElementById('three'))
-let time4pm = $(document.createElementById('four'))
-let time5pm = $(document.createElementById('five'))
+let time9am = $(document.getElementById('nine'))
+let time10am = $(document.getElementById('ten'))
+let time11am = $(document.getElementById('eleven'))
+let time12pm = $(document.getElementById('tweleve'))
+let time1pm = $(document.getElementById('one'))
+let time2pm = $(document.getElementById('two'))
+let time3pm = $(document.getElementById('three'))
+let time4pm = $(document.getElementById('four'))
+let time5pm = $(document.getElementById('five'))
 
 function timeUpdate() {
-    let today =moment();
+    let today = moment();
 
     // update the time elemennt in the header
     $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm.ss"));
 
-    // for colorging the past,  present, and future time blocks
+    // for coloring the past,  present, and future time blocks
     let now = moment().format("kk");
     for (let i = 0; i < scheduleTime.length; i++) {
         scheduleTime[i].removeClass("present past future");
@@ -46,7 +46,7 @@ let scheduleTime = [
     time5pm,
 ];
 
-saveDate();
+saveData();
 timeUpdate();
 setInterval(timeUpdate, 1000);
 
@@ -61,12 +61,12 @@ function handleSubmit(e) {
 
     let btnClicked = $(e.currentTarget);
     
-    let targetText =btnClicked.siblings(textarea);
+    let targetText =btnClicked.siblings("textarea");
 
     let targetTimeBlock = targetText.data("hour");
 
     localStorage.setItem("time block " + targetTimeBlock, targetText.val());
 }
 
-saveIconBtn.on("click", handSubmit);
+saveIconBtn.on("click", handleSubmit);
 
